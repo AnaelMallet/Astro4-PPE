ad9a4941771cba2adfb0ca17e031cd121d9db0b0
Anaël <anael.mallet@gmail.com>
2019-04-06 17:51:52 +0100

    Astro4: debug commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,44 @@
 <template>
     <v-app class="app">
+        <v-navigation-drawer v-model="drawer" class="elevation-12" app temporary>
+            <myContentDrawer/>
+        </v-navigation-drawer>
         <v-toolbar class="blue darken-4">
-            <v-menu></v-menu>
-            <v-btn small class ="grey lighten-2" v-on:click="GoConn()">Connexion</v-btn>
-            <v-btn small class ="grey lighten-2" v-on:click="GoInscr()">Inscription</v-btn>
-            <v-btn small class ="grey lighten-2" v-on:click="GoCons()">Consultation</v-btn>
+            <v-toolbar-side-icon dark v-on:click="drawer = true" v-if="this.$session.exists()"></v-toolbar-side-icon><div class="white--text menu">{{this.$session.get('name')}}</div>
+            <v-btn small class ="grey lighten-2" v-on:click="Disconnect()" v-if="this.$session.exists()">Déconnexion</v-btn>
+            <v-btn small class ="grey lighten-2" v-on:click="GoConn()" v-else>Connexion</v-btn>
+            <v-btn small class ="grey lighten-2" v-on:click="GoInscr()" v-if="!this.$session.exists()">Inscription</v-btn>
         </v-toolbar>
         <div>
             <v-parallax height="250"
                         src="https://hdfondsdecran.com/image/201609/338/espace-profond-hyperespace-pourpre.jpg">
             </v-parallax>
         </div>
+        <v-dialog v-model="disconnect" max-width="450">
+            <v-card>
+                <v-card-title class="green">
+                    <h1>Vous êtes Déconnecté</h1>
+                </v-card-title>
+                <v-card-text>
+                    <h3>Au revoir</h3>
+                </v-card-text>
+            </v-card>
+        </v-dialog>
     </v-app>
 </template>
 
 <script>
+    import myContentDrawer from "@/components/drawer"
+    
     export default {
+        components: {myContentDrawer},
+        data() {
+            return {
+                disconnect: false,
+                drawer: false,
+            }
+        },
+
         methods: {
             GoConn() {
                 this.$router.push('/connexion')
@@ -24,8 +47,18 @@
             GoInscr() {
                 this.$router.push('/inscription')
             },
-            GoCons(){
-                this.$router.push('/consultation')
+
+            Disconnect() {
+                this.$session.destroy()
+                this.disconnect = true
+                if (this.$route.path === "/consultation" || this.$route.path === "/planning")
+                {
+                    this.$router.push('/')
+                }
+            },
+
+            GoMain() {
+                this.$router.push('/')
             }
         }
     }
@@ -38,5 +71,13 @@
 
     .app {
         height: 300px;
+    }
+
+    .menu {
+        margin-right: 15px;
+    }
+
+    .MainBtn{
+        right: -1530px;
     }
 </style>
